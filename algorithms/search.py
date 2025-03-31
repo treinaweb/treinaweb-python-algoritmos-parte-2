a1a35ee9ed46371234192b6f463705517b785c33
Cleyson Lima <cleysonph@gmail.com>
2025-03-30 23:26:59 -0300

    02:03 - Jump search

--- algorithms/search.py
+++ algorithms/search.py
@@ -16,3 +16,21 @@ def binary_search(arr: list[int], target: int) -> int:
         else:
             right = mid - 1
     return -1
+
+
+def jump_search(arr: list[int], tagert: int) -> int:
+    n = len(arr)
+    step = int(n**0.5)
+    prev = 0
+    while arr[min(step, n) - 1] < tagert:
+        prev = step
+        step += int(n**0.5)
+        if prev >= n:
+            return -1
+    while arr[prev] < tagert:
+        prev += 1
+        if prev == min(step, n):
+            return -1
+    if arr[prev] == tagert:
+        return prev
+    return -1
---
+++ main.py
@@ -0,0 +1,6 @@
+from algorithms.search import jump_search
+
+arr = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+target = -1
+
+print(jump_search(arr, target))
